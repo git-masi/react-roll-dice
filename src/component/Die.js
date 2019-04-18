bc48b539c16211e2e3d587e505fc46f03f4f1009
Eric Masi <eric@gitmasi.com>
2019-04-18 14:18:32 -0400

    add basic styles and layout for all components

--- src/component/Die.js
+++ src/component/Die.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './Die.css';
 
 const die = props => {
--- src/component/RollDice.js
+++ src/component/RollDice.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import Die from './Die';
+import './RollDice.css';
 
 export default class RollDice extends Component {
   render() {
     return (
-      <div>
-        <Die number="two"/>
-        <Die number="five"/>
+      <div className="RollDice">
+        <div className="RollDice--dice-container">
+          <Die number="two"/>
+          <Die number="five"/>
+        </div>
+        <button className="RollDice--btn">Roll Dice!</button>
       </div>
     )
   }
