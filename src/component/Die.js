0231bf2126cd7c285242446eb2ecf4b6f37c43e3
Eric Masi <eric@gitmasi.com>
2019-04-18 16:07:44 -0400

    make die comp. conditional styling more readable

--- src/component/Die.js
+++ src/component/Die.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Die.css';
 
 const die = props => {
-  const classes = ["Die", props.rolling ? "wobble-hor-bottom" : null];
+  const classes = ["Die", props.rolling && "wobble-hor-bottom"].join(' ');
   return (
-    <div className={classes.join(' ')}>
+    <div className={classes}>
       <i className={`fas fa-dice-${props.number}`}></i>
     </div>
   )
