e8c7531121020abb1a73a73ceef98797216d3241
Eric Masi <eric@gitmasi.com>
2019-04-18 13:38:36 -0400

    add app components, add font awesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Die from './component/Die';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <Die/>
     );
   }
 }
---
+++ src/component/Die.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const die = props => {
+  return (
+    <div></div>
+  )
+}
+
+export default die;
